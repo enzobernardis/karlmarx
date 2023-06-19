009d2b5f7c6d2c5f3296a69973f89ee360076a85
enzobernardis <131674025+enzobernardis@users.noreply.github.com>
2023-06-19 08:32:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let proximo = 0;
+const imagens = [
+    "img/img3.jpg",
+    "img/img2.webp",
+    "img/img1.webp"
+];
+
+function Trocar(vez) {
+    let img = window.document.getElementById("img");
+
+    img.classList.add("fade-out");
+
+    setTimeout(function () {
+        proximo = (proximo + vez) % imagens.length;
+
+        if (proximo < 0) {
+            proximo = imagens.length - 1;
+        }
+
+        img.src = imagens[proximo];
+        img.classList.remove("fade-out");
+    }, 250); 
+}
+
+function Validar(event) {
+    text1 = window.document.getElementById("nome").value;
+    text2 = window.document.getElementById("email").value;
+    text3 = window.document.getElementById("mensagem").value;
+
+    if(text1 == ""){
+        alert("Preencha seu nome");
+        text1.focus();
+        event.preventDefault();
+        return false;
+    } 
+    else if(text2 == ""){
+        alert("Preencha seu email");
+        text2.focus();
+        event.preventDefault();
+        return false;
+    }
+    else if(text3 == ""){
+        alert("Coloque seu comentario");
+        text3.focus();
+        event.preventDefault();
+        return false;
+    }
+}
